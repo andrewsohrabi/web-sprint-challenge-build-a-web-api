c1cfcc6b87bc3958fb70a0843027e104eb570989
andrewsohrabi <andrew.sohrabi@gmail.com>
2021-03-12 15:43:52 -0800

    set up routers and got express server up

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const server = express();
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+
+const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
+
+const server = express();
+server.use(express.json());
+
+server.use('/api/actions',actionsRouter);
+server.use('/api/projects',projectsRouter);
 
 module.exports = server;
