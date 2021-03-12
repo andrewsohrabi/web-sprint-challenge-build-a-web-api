c0084db76975abf953eb6dac3ec6a5f5717640f2
andrewsohrabi <andrew.sohrabi@gmail.com>
2021-03-12 15:51:59 -0800

    GET all actions works

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,4 +3,14 @@ const express = require('express');
 const router = express.Router();
 const Actions = require('./actions-model');
 
+//[GET] /api/actions
+router.get('/', (req,res) => {
+    Actions.get()
+        .then(actions => {
+            res.status(200).json(actions)
+        }).catch(err => {
+            res.status(500).json({message: 'error getting actions'})
+        })
+})
+
 module.exports = router;
