3f5602c706d6c35432999e03cee417432ad3dab2
andrewsohrabi <andrew.sohrabi@gmail.com>
2021-03-12 16:33:09 -0800

    [GET] /api/projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,6 +3,17 @@ const express = require('express');
 const router = express.Router();
 const Projects = require('./projects-model');
 
+// [GET] /api/projects` returns an array of projects (or an empty array) as the body of the response.
+router.get('/', (req,res)=> {
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects)
+        }).catch(err => {
+            res.status(500).json({message: 'error fetching projects'})
+        })
+})
+
+
 
 
 
