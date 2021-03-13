af6dcad6dc1c6bb01d677c0c2071b6d2c5fa1a66
andrewsohrabi <andrew.sohrabi@gmail.com>
2021-03-12 16:30:11 -0800

    [DELETE] /api/actions/:id was previous commit

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,7 +1,9 @@
-// Write your "projects" router here!
+// Write your "projects" router here!!
 const express = require('express');
 const router = express.Router();
 const Projects = require('./projects-model');
+
+
 
 
 module.exports = router;
